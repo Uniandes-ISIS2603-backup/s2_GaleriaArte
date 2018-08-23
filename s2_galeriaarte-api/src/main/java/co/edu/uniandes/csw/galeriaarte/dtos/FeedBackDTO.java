fac6a2f6b02bb7f44424f55479917ebcfdd021f7
s.restrepos1 <s.restrepos1@ISC408.sis.virtual.uniandes.edu.co>
2018-08-23 14:59:51 -0500

    feedbackDTO

---
+++ s2_galeriaarte-api/src/main/java/co/edu/uniandes/csw/galeriaarte/dtos/FeedBackDTO.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.galeriaarte.dtos;
+
+/**
+ *
+ * @author s.restrepos1
+ */
+public class FeedBackDTO 
+{
+    private Long id;
+    
+    private String name;
+    
+    //private ObraDTO obra;
+    
+    //private UserDTO usuario;
+    
+    public FeedBackDTO()
+    {
+        
+    }
+}
---
+++ s2_galeriaarte-api/src/main/java/co/edu/uniandes/csw/galeriaarte/resources/FeedBackResources.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.galeriaarte.resources;
+
+/**
+ *
+ * @author s.restrepos1
+ */
+import co.edu.uniandes.csw.galeriaarte.dtos.FeedBackDTO;
+//@Path()
+//@Procedures()
+//@Consumes()
+//@RequestedScoped
+public class FeedBackResources 
+{
+    //@Inject
+    //FeedBackLogic feedBackLogic;
+    
+    //@POST
+    public FeedBackDTO createFeedBack(FeedBackDTO fdbDTO)
+    {
+        return fdbDTO;
+    }
+}
