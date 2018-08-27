0ef6a666226d0370037a0c383046606c610943da
estudiante <estudiante@ISIS2603-365.sis.virtual.uniandes.edu.co>
2018-08-26 21:01:59 -0500

    MedioPagoDto, resource y entity terminados

---
+++ s2_galeriaarte-back/src/main/java/co/edu/uniandes/csw/galeriaarte/entities/MedioPagoEntity.java
@@ -0,0 +1,71 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package co.edu.uniandes.csw.galeriaarte.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ * Clase que representa un medio de pago  en la persistencia y permite su
+ * serialización.
+ *
+ * @author ja.penat
+ */
+@Entity
+public class MedioPagoEntity extends BaseEntity implements Serializable
+{
+    private String description;
+    private Long number;
+    private String bank;
+    
+    /**
+     * @return the description
+     */
+    public String getDescription()
+    {
+        return description;
+    }
+    
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+    
+    /**
+     * @return the number
+     */
+    public Long getNumber()
+    {
+        return number;
+    }
+    
+    /**
+     * @param number the number to set
+     */
+    public void setNumber(Long number)
+    {
+        this.number = number;
+    }
+    
+    /**
+     * @return the bank
+     */
+    public String getBank()
+    {
+        return bank;
+    }
+    
+    /**
+     * @param bank the bank to set
+     */
+    public void setBank(String bank)
+    {
+        this.bank = bank;
+    }
+}
