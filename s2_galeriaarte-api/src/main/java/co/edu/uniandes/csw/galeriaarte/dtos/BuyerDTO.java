bc1ec0d19dd2ac16e3ce44a2bd4dc9c23e1550c6
sacostav <42441785+sacostav@users.noreply.github.com>
2018-08-29 08:01:54 -0500

    Add files via upload

---
+++ s2_galeriaarte-api/src/main/java/co/edu/uniandes/csw/galeriaarte/dtos/BuyerDTO.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.galeriaarte.dtos;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author estudiante
+ */
+public class Buyer {
+    
+    public Buyer(){
+        
+    }
+    
+    /**
+     * Compra una pintura
+     * @return 
+     */
+    
+    public Boolean buyPaintWork(){
+        
+        return false;
+    }
+    
+    /**
+     * Busca una pintura en la galeria 
+     */
+    
+    public void seekPaintWork(){
+        
+    }
+    
+    /**
+     * Agrega una pintura a la lista de favoritas 
+     */
+    
+    public void addFavoritePaintWork(){
+        
+    }
+    
+    /**
+     * Retorna la lista de pinturas favoritas
+     * @return
+     */
+    
+    public ArrayList getFavoritePaintworks(){
+        return null;
+    }
+}
+
