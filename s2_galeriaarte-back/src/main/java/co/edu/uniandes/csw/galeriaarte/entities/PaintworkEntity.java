699cbfd186ce088c52fa1b4aab83492bb3aaa002
estudiante <estudiante@ISIS2603-351.sis.virtual.uniandes.edu.co>
2018-09-06 17:32:03 -0500

    haciendo paintwork

--- s2_galeriaarte-back/src/main/java/co/edu/uniandes/csw/galeriaarte/entities/PaintworkEntity.java
+++ s2_galeriaarte-back/src/main/java/co/edu/uniandes/csw/galeriaarte/entities/PaintworkEntity.java
@@ -15,5 +15,129 @@ import javax.persistence.Entity;
 @Entity
 class PaintworkEntity extends BaseEntity implements Serializable
 {
+    private String name;
+    private String country;
+    private KindDTO kind;
+    private CategoryDTO category;
+    private FeedBackEntity feedback;
+    private String description;
+    private Long value;
+    private Boolean verificacionObra;
+    private String imagePath;
+    private String videoPath;
+
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the country
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     * @param country the country to set
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     * @return the feedback
+     */
+    public FeedBackEntity getFeedback() {
+        return feedback;
+    }
+
+    /**
+     * @param feedback the feedback to set
+     */
+    public void setFeedback(FeedBackEntity feedback) {
+        this.feedback = feedback;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the value
+     */
+    public Long getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(Long value) {
+        this.value = value;
+    }
+
+    /**
+     * @return the verificacionObra
+     */
+    public Boolean getVerificacionObra() {
+        return verificacionObra;
+    }
+
+    /**
+     * @param verificacionObra the verificacionObra to set
+     */
+    public void setVerificacionObra(Boolean verificacionObra) {
+        this.verificacionObra = verificacionObra;
+    }
+
+    /**
+     * @return the imagePath
+     */
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    /**
+     * @param imagePath the imagePath to set
+     */
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    /**
+     * @return the videoPath
+     */
+    public String getVideoPath() {
+        return videoPath;
+    }
+
+    /**
+     * @param videoPath the videoPath to set
+     */
+    public void setVideoPath(String videoPath) {
+        this.videoPath = videoPath;
+    }
+    
     
 }
