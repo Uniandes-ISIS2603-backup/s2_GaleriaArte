bf9c906b95e00ba4cb09f923c6a4e08ca3e57053
LauraManrique <>
2018-09-10 00:35:36 -0500

    Persistence, logic, entity v1

---
+++ s2_galeriaarte-back/src/main/java/co/edu/uniandes/csw/galeriaarte/entities/CVEntity.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.galeriaarte.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import uk.co.jemos.podam.common.PodamExclude;
+
+
+/**
+ *
+ * @author estudiante
+ */
+
+@Entity
+public class CVEntity extends BaseEntity implements Serializable{
+    
+        
+@PodamExclude
+@OneToOne(mappedBy="cv", fetch=FetchType.LAZY)
+private ArtistEntity artist;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+     private String name;
+     private String educationDTO;
+     private PaintworkEntity obraMasConocidaDTO;
+     private String fechaNacimientoDTO;
+     private String informacionAdicionalDTO;
+     private ArrayList obrasDTO;
+      /**
+     * Retorna el nivel de educación del artista
+     *
+     * @return nivel de educación del artista
+     */
+    public String geteducationDTO() {
+        return educationDTO;
+    }
+
+    /**
+     * Modifica el nivel de educación del artista
+     *
+     * @param educationDTO
+     */
+    public void seteducationDTO(String educationDTO) {
+        this.educationDTO = educationDTO;
+    }
+
+    /**
+     * Retorna la Obra más conocida del artista
+     *
+     * @return Obra más conocida del artista
+     */
+    public PaintworkEntity getobraMasConocidaDTO() {
+        return obraMasConocidaDTO;
+    }
+
+    /**
+     * Modifica la Obra más conocida del artista
+     *
+     * @param obraMasConocidaDTO
+     */
+    public void setobraMasConocidaDTO(PaintworkEntity obraMasConocidaDTO) {
+        this.obraMasConocidaDTO = obraMasConocidaDTO;
+    }
+
+    /**
+     * Retorna la Fecha de nacimiento del artista
+     *
+     * @return Fecha de nacimiento del artista
+     */
+    public String getfechaNacimientoDTO() {
+        return fechaNacimientoDTO;
+    }
+
+    /**
+     * Modifica la Fecha de nacimiento del artista
+     *
+     * @param fechaNacimientoDTO
+     */
+    public void setfechaNacimientoDTO(String fechaNacimientoDTO) {
+        this.fechaNacimientoDTO = fechaNacimientoDTO;
+    }
+
+    /**
+     * Retorna la Información adicional sobre el artista
+     *
+     * @return Información adicional sobre el artista
+     */
+    public String getinformacionAdicionalDTO() {
+        return informacionAdicionalDTO;
+    }
+
+    /**
+     * Modifica la Información adicional sobre el artista
+     *
+     * @param informacionAdicionalDTO
+     */
+    public void setinformacionAdicionalDTO(String informacionAdicionalDTO) {
+        this.informacionAdicionalDTO = informacionAdicionalDTO;
+    }
+
+    /**
+     * Retorna las obrasDTO del artista
+     *
+     * @return las obrasDTO del artista
+     */
+    public ArrayList getobrasDTO() {
+        return obrasDTO;
+    }
+
+    /**
+     * Modifica las obrasDTO del artista
+     *
+     * @param informacionAdicionalDTO
+     */
+    public void setobrasDTO(ArrayList obrasDTO) {
+        this.obrasDTO = obrasDTO;
+    }
+}
+
