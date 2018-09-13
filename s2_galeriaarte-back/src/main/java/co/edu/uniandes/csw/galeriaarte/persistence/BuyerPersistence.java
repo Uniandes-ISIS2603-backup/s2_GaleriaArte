54b641d6bceba462731e85a62304fc4b3a1c8119
sacostav <s.acostav@uniandes.edu.co>
2018-09-12 23:01:23 -0500

    Cambios buyerPersistence

--- s2_galeriaarte-back/src/main/java/co/edu/uniandes/csw/galeriaarte/persistence/BuyerPersistence.java
+++ s2_galeriaarte-back/src/main/java/co/edu/uniandes/csw/galeriaarte/persistence/BuyerPersistence.java
@@ -5,15 +5,32 @@
  */
 package co.edu.uniandes.csw.galeriaarte.persistence;
 import co.edu.uniandes.csw.galeriaarte.entities.BuyerEntity;
+import javax.ejb.Stateless;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
- * @author estudiante
+ * @author sara acosta
  */
+
+@Stateless
+
 public class BuyerPersistence {
     
-    public void create(BuyerEntity buyerEntity){
+    private static final Logger LOGGER = Logger.getLogger(BuyerPersistence.class.getName());
+    
+    @PersistenceContext(unitName = "InterArtPU")
+    protected EntityManager em;
+    
+    public BuyerEntity create(BuyerEntity buyerEntity){
+        LOGGER.log(Level.INFO, "Creando un comprador nuevo");
         
+        em.persist(buyerEntity);
+        LOGGER.log(Level.INFO, "Saliendo de crear un comprador");
+        return buyerEntity;
     }
     
 }
