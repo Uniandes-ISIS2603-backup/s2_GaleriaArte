51e5d895419646aae92051270baef07f6691d294
a.barragan <a.barragan@uniandes.edu.co>
2018-11-13 22:51:46 -0500

    un pequeño cambio para verificar el reset

--- s2_galeriaarte-back/src/main/java/co/edu/uniandes/csw/galeriaarte/entities/ArtistEntity.java
+++ s2_galeriaarte-back/src/main/java/co/edu/uniandes/csw/galeriaarte/entities/ArtistEntity.java
@@ -28,7 +28,7 @@ public class ArtistEntity extends BaseEntity implements Serializable
     private CVEntity hojaDeVida;
     
     @PodamExclude
-    @OneToMany()
+    @OneToMany(mappedBy="artist", fetch=FetchType.LAZY)
     private List<PaintworkEntity> obras;
     
        
