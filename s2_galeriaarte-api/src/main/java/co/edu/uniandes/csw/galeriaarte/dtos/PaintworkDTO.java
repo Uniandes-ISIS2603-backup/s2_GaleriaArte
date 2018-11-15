2b1bb68cc92ee7387871201725a7a8180ab496d5
a.barragan <a.barragan@uniandes.edu.co>
2018-11-15 08:29:25 -0500

    clean and build working

--- s2_galeriaarte-api/src/main/java/co/edu/uniandes/csw/galeriaarte/dtos/PaintworkDTO.java
+++ s2_galeriaarte-api/src/main/java/co/edu/uniandes/csw/galeriaarte/dtos/PaintworkDTO.java
@@ -6,15 +6,15 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 
 /**
  *
- * @author aquí irá el usuario/nombre de quien lo tenga que hacer (supongo)
+ * @author Anderson
  */
 public class PaintworkDTO implements Serializable {
     private Long idPaintwork;
     private String name;
     private String country;
-    private KindDTO kind;
-    private CategoryDTO category;
-    private FeedBackDTO feedback;
+    //private KindDTO kind;
+    //private CategoryDTO category;
+    //private FeedBackDTO feedback;
     private String description;
     private Long value;
     private Boolean verificacionObra;
@@ -28,18 +28,16 @@ public class PaintworkDTO implements Serializable {
      * Conviertir Entity a DTO (Crea un nuevo DTO con los valores que recibe en
      * la entidad que viene de argumento.
      *
-     * @param paintwirkEntity: Es la entidad que se va a convertir a DTO
+     * @param paintworkEntity: Es la entidad que se va a convertir a DTO
      */
-    
-    
     public PaintworkDTO (PaintworkEntity paintworkEntity) {
         if (paintworkEntity != null) {
             this.idPaintwork = paintworkEntity.getId();
             this.name = paintworkEntity.getName();
             this.country=paintworkEntity.getCountry();
-            this.kind = new KindDTO();
-            this.category=new CategoryDTO();
-            this.feedback = new FeedBackDTO();
+            //this.kind = new KindDTO();
+            //this.category=new CategoryDTO();
+            //this.feedback = new FeedBackDTO();
             this.description = paintworkEntity.getDescription();
             this.value = paintworkEntity.getValor();
             this.verificacionObra = paintworkEntity.getVerificacionObra();
@@ -95,44 +93,44 @@ public class PaintworkDTO implements Serializable {
     /**
      * @return the kind
      */
-    public KindDTO getKind() {
-        return kind;
-    }
+    //public KindDTO getKind() {
+    //    return kind;
+    //}
 
     /**
      * @param kind the kind to set
      */
-    public void setKind(KindDTO kind) {
-        this.kind = kind;
-    }
+    //public void setKind(KindDTO kind) {
+    //    this.kind = kind;
+    //}
 
     /**
      * @return the category
      */
-    public CategoryDTO getCategory() {
-        return category;
-    }
+    //public CategoryDTO getCategory() {
+    //    return category;
+    //}
 
     /**
      * @param category the category to set
      */
-    public void setCategory(CategoryDTO category) {
-        this.category = category;
-    }
+    //public void setCategory(CategoryDTO category) {
+    //    this.category = category;
+    //}
 
     /**
      * @return the feedback
      */
-    public FeedBackDTO getFeedback() {
-        return feedback;
-    }
+    //public FeedBackDTO getFeedback() {
+    //    return feedback;
+    //}
 
     /**
      * @param feedback the feedback to set
      */
-    public void setFeedback(FeedBackDTO feedback) {
-        this.feedback = feedback;
-    }
+    //public void setFeedback(FeedBackDTO feedback) {
+    //    this.feedback = feedback;
+    //}
 
     /**
      * @return the description
@@ -230,7 +228,6 @@ public class PaintworkDTO implements Serializable {
     public String toString() {
         return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
     }
-    
 }
 
 
