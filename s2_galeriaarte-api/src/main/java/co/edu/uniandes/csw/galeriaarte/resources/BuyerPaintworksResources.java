250921045d2d09b065f40e15176382549c1a9dca
sacostav <s.acostav@uniandes.edu.co>
2018-11-26 21:55:52 -0500

    Asc Buyer y Paintwork

---
+++ s2_galeriaarte-api/src/main/java/co/edu/uniandes/csw/galeriaarte/resources/BuyerPaintworksResources.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.galeriaarte.resources;
+import co.edu.uniandes.csw.galeriaarte.dtos.PaintworkDTO;
+import co.edu.uniandes.csw.galeriaarte.dtos.PaintworkDetailDTO;
+import co.edu.uniandes.csw.galeriaarte.ejb.BuyerLogic;
+import co.edu.uniandes.csw.galeriaarte.entities.PaintworkEntity;
+import co.edu.uniandes.csw.galeriaarte.ejb.BuyerPaintworksLogic;
+import co.edu.uniandes.csw.galeriaarte.ejb.PaintworkLogic;
+import java.util.List;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.WebApplicationException;
+import co.edu.uniandes.csw.galeriaarte.exceptions.BusinessLogicException;
+/**
+ *
+ * @author estudiante
+ */
+
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class BuyerPaintworksResources {
+    
+    
+    private static final Logger LOGGER = Logger.getLogger(BuyerPaintworksResources.class.getName());
+
+    @Inject
+    private BuyerPaintworksLogic buyerPaintworksLogic;
+
+    @Inject
+    private PaintworkLogic paintworkLogic; // Variable para acceder a la lógica de la aplicación. Es una inyección de dependencias
+    
+    @POST
+    @Path("{paintworksId: \\d+}")
+    public PaintworkDTO addPaintwork(@PathParam("buyersId") Long buyersId, @PathParam("paintworksId") Long paintworksId) {
+        LOGGER.log(Level.INFO, "BuyerPaintworksResource addPaintwork: input: buyersID: {0} , paintworksId: {1}", new Object[]{buyersId, paintworksId});
+        if (paintworkLogic.getPaintWork(paintworksId) == null) {
+            throw new WebApplicationException("El recurso /paintworks/" + paintworksId + " no existe.", 404);
+        }
+        
+        PaintworkDTO paintworkDTO = new PaintworkDTO(buyerPaintworksLogic.addPaintwork(buyersId, paintworksId));
+        LOGGER.log(Level.INFO, "BuyerPaintworksResource addBook: output: {0}", paintworkDTO);
+        return paintworkDTO;
+    }
+    
+    @GET
+    public List<PaintworkDetailDTO> getPaintworks(@PathParam("buyersId") Long buyersId) {
+        LOGGER.log(Level.INFO, "BuyerPaintworksResource getPaintworks: input: {0}", buyersId);
+        List<PaintworkDetailDTO> listaDetailDTOs = paintworksListEntity2DTO(buyerPaintworksLogic.getPaintworks(buyersId));
+        LOGGER.log(Level.INFO, "BuyerPaintworksResource getPaintworks: output: {0}", listaDetailDTOs);
+        return listaDetailDTOs;
+    }
+    
+    @GET
+    @Path("{paintworksId: \\d+}")
+    public PaintworkDetailDTO getPaintwork(@PathParam("buyersId") Long buyersId, @PathParam("paintworksId") Long paintworksId) throws BusinessLogicException {
+        LOGGER.log(Level.INFO, "BuyerPaintworksResource getPaintwork: input: buyersID: {0} , paintworksId: {1}", new Object[]{buyersId, paintworksId});
+        if (paintworkLogic.getPaintWork(paintworksId) == null) {
+            throw new WebApplicationException("El recurso /buyers/" + buyersId + "/paintworks/" + paintworksId + " no existe.", 404);
+        }
+        PaintworkDetailDTO paintworkDetailDTO = new PaintworkDetailDTO(buyerPaintworksLogic.getPaintwork(buyersId, paintworksId));
+        LOGGER.log(Level.INFO, "BuyerPaintworksResource getPaintwork: output: {0}", paintworkDetailDTO);
+        return paintworkDetailDTO;
+    }
+    
+     @PUT
+    public List<PaintworkDetailDTO> replacePaintworks(@PathParam("buyersId") Long buyersId, List<PaintworkDetailDTO> paintworks) {
+        LOGGER.log(Level.INFO, "BuyerPaintworksResource replacePaintworks: input: buyersId: {0} , paintworks: {1}", new Object[]{buyersId, paintworks});
+        for (PaintworkDetailDTO paintwork : paintworks) {
+            if (paintworkLogic.getPaintWork(paintwork.getIdPaintwork()) == null) {
+                throw new WebApplicationException("El recurso /paintworks/" + paintwork.getIdPaintwork() + " no existe.", 404);
+            }
+        }
+        List<PaintworkDetailDTO> listaDetailDTOs = paintworksListEntity2DTO(buyerPaintworksLogic.replacePaintworks(buyersId, paintworksListDTO2Entity(paintworks)));
+        LOGGER.log(Level.INFO, "BuyerPaintworksResource replacePaintworks: output: {0}", listaDetailDTOs);
+        return listaDetailDTOs;
+    }
+    
+    private List<PaintworkDetailDTO> paintworksListEntity2DTO(List<PaintworkEntity> entityList) {
+        List<PaintworkDetailDTO> list = new ArrayList();
+        for (PaintworkEntity entity : entityList) {
+            list.add(new PaintworkDetailDTO(entity));
+        }
+        return list;
+    }
+    
+    private List<PaintworkEntity> paintworksListDTO2Entity(List<PaintworkDetailDTO> dtos) {
+        List<PaintworkEntity> list = new ArrayList<>();
+        for (PaintworkDetailDTO dto : dtos) {
+            list.add(dto.toEntity());
+        }
+        return list;
+    }
+}
---
+++ s2_galeriaarte-back/src/main/java/co/edu/uniandes/csw/galeriaarte/ejb/BuyerPaintworksLogic.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.galeriaarte.ejb;
+import java.util.List;
+import co.edu.uniandes.csw.galeriaarte.persistence.BuyerPersistence;
+import co.edu.uniandes.csw.galeriaarte.persistence.PaintworkPersistence;
+import co.edu.uniandes.csw.galeriaarte.entities.BuyerEntity;
+import co.edu.uniandes.csw.galeriaarte.entities.PaintworkEntity;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import co.edu.uniandes.csw.galeriaarte.exceptions.BusinessLogicException;
+/**
+ *
+ * @author estudiante
+ */
+public class BuyerPaintworksLogic {
+    
+     private static final Logger LOGGER = Logger.getLogger(BuyerPaintworksLogic.class.getName());
+
+    @Inject
+    private PaintworkPersistence paintworkPersistence;
+
+    @Inject
+    private BuyerPersistence buyerPersistence;
+
+    /**
+     * Agregar un  paintwork a elbuyer
+     *
+     * @param paintworksId El id paintwork a guardar
+     * @param buyersId El id de el comprador en elcual se va a guardar el
+     * paintwork.
+     * @return elobracreado.
+     */
+    public PaintworkEntity addPaintwork(Long paintworksId, Long buyersId) {
+        LOGGER.log(Level.INFO, "Inicia proceso de agregarle un  paintwork a el comprador con id = {0}", buyersId);
+        BuyerEntity buyerEntity = buyerPersistence.find(buyersId);
+        PaintworkEntity paintworkEntity = paintworkPersistence.find(paintworksId);
+        paintworkEntity.setBuyer(buyerEntity);
+        LOGGER.log(Level.INFO, "Termina proceso de agregarle un  paintwork a el comprador con id = {0}", buyersId);
+        return paintworkEntity;
+    }
+
+    /**
+     * Retorna todos los paintworks asociados a un a buyer
+     *
+     * @param buyersId El ID de el comprador buscada
+     * @return ellista de paintworks de elbuyer
+     */
+    public List<PaintworkEntity> getPaintworks(Long buyersId) {
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar los paintworks asociados a el comprador con id = {0}", buyersId);
+        return buyerPersistence.find(buyersId).getPaintworks();
+    }
+
+    /**
+     * Retorna un  paintwork asociado a un a buyer
+     *
+     * @param buyersId El id de el comprador a buscar.
+     * @param paintworksId El id delobraa buscar
+     * @return elobraencontrado dentro de elbuyer.
+     * @throws BusinessLogicException Si elobrano se encuentra en la
+     * buyer
+     */
+    public PaintworkEntity getPaintwork(Long buyersId, Long paintworksId) throws BusinessLogicException {
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar elobracon id = {0} de el comprador con id = " + buyersId, paintworksId);
+        List<PaintworkEntity> paintworks = buyerPersistence.find(buyersId).getPaintworks();
+        PaintworkEntity paintworkEntity = paintworkPersistence.find(paintworksId);
+        int index = paintworks.indexOf(paintworkEntity);
+        LOGGER.log(Level.INFO, "Termina proceso de consultar elobracon id = {0} de el comprador con id = " + buyersId, paintworksId);
+        if (index >= 0) {
+            return paintworks.get(index);
+        }
+        throw new BusinessLogicException("elobrano está asociado a elbuyer");
+    }
+
+    /**
+     * Remplazar paintworks de un a buyer
+     *
+     * @param paintworks Lista de paintworks que serán los de elbuyer.
+     * @param buyersId El id de el comprador que se quiere actualizar.
+     * @return ellista de paintworks actualizada.
+     */
+    public List<PaintworkEntity> replacePaintworks(Long buyersId, List<PaintworkEntity> paintworks) {
+        LOGGER.log(Level.INFO, "Inicia proceso de actualizar el comprador con id = {0}", buyersId);
+        BuyerEntity buyerEntity = buyerPersistence.find(buyersId);
+        List<PaintworkEntity> paintworkList = paintworkPersistence.findAll();
+        for (PaintworkEntity paintwork : paintworkList) {
+            if (paintworks.contains(paintwork)) {
+                paintwork.setBuyer(buyerEntity);
+            } else if (paintwork.getBuyer() != null && paintwork.getBuyer().equals(buyerEntity)) {
+                paintwork.setBuyer(null);
+            }
+        }
+        LOGGER.log(Level.INFO, "Termina proceso de actualizar el comprador con id = {0}", buyersId);
+        return paintworks;
+    }
+}
